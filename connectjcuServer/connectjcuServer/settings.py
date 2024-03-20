2f8434187c7ea578fedb080d8c03f6a75e0a7975
Ar Kar <arkar@Ars-MacBook-Air.local>
2024-03-20 15:17:06 +0800

    added vercel host as allowed

--- connectjcuServer/connectjcuServer/settings.py
+++ connectjcuServer/connectjcuServer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "true"
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['0.0.0.0','localhost','45.32.108.232']
+ALLOWED_HOSTS = ['0.0.0.0','localhost','45.32.108.232','https://connectjcu-client.vercel.app']
 
 
 # Application definition
@@ -57,8 +57,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "connectjcuServer.urls"
-CORS_URLS_REGEX = r"^/api/.*" 
+# CORS_URLS_REGEX = r"^/api/.*" 
 CORS_ALLOWED_ORIGINS = [
+    'https://connectjcu-client.vercel.app',
+    'http://localhost:3000',
+    'https://localhost:3000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://connectjcu-client.vercel.app',
     'http://localhost:3000',
     'https://localhost:3000',
 ]
@@ -160,7 +167,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
