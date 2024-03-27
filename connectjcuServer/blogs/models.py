954fbcaaa8cd154e7f98a008475b5ec24faa5a72
Ar Kar <arkar@Ars-MacBook-Air.local>
2024-03-27 14:24:44 +0800

    added default profile and banner

--- connectjcuServer/blogs/models.py
+++ connectjcuServer/blogs/models.py
@@ -7,7 +7,7 @@ from categories.models import Category
 class Blog(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
-    image_file = models.ImageField(upload_to='images/', blank=True, default='default.jpg')
+    image_file = models.ImageField(upload_to='images/', blank=True, default='images/default_blogbanner.jpg')
     view_count = models.IntegerField(default=0)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
